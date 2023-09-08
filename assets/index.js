1512fee79bc09c5f79e22fd440d371e00abb5b74
swiTTcHH <dvdokpeta@gmail.com>
2023-09-08 17:23:19 +0100

    reup

--- assets/index.js
+++ assets/index.js
@@ -1,9 +1,27 @@
-const ids = document.querySelectorAll("[data-testid]");
-
-        ids.forEach(id => {
-            let today = new Date();
-            if (id.dataset.testid === "currentDayOfTheWeek"){
-                id.innerHTML = today.toUTCString();
-            }
-            
-        })
+function getWeekDay() {
+    const date = new Date();
+    const weekDays = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    return weekDays[date.getDay()];
+}
+function getUTCTime() {
+    return new Date().getTime();
+}
+function updateUTCTime() {
+    const utcTime = document.querySelector("[data-testid='CurrentUTCTime']");
+    utcTime.innerHTML = `${getUTCTime()}`;
+    setInterval(() => (utcTime.innerHTML = `${getUTCTime()}`), 1000);
+}
+function updateWeekDay() {
+    const weekDay = document.querySelector("[data-testid='currentDayOfTheWeek']");
+    weekDay.innerHTML = `${getWeekDay()}`;
+}
+updateWeekDay();
+updateUTCTime();
